46cc0bdbbb90195307ac5067ba494ee2673b00c3
Dustin Swales <dustin.swales@noaa.gov>
2026-06-30 22:56:17 +0000

    Some more changes. WIP

--- src/core_atmosphere/ccpp/CCPP_driver.F90
+++ src/core_atmosphere/ccpp/CCPP_driver.F90
@@ -7,7 +7,7 @@ module CCPP_driver
                                 ccpp_physics_timestep_final, &
                                 ccpp_physics_final,          &
                                 ccpp_final
-
+  use iso_fortran_env,    only: error_unit
   implicit none
 
   integer :: mythread
@@ -20,12 +20,78 @@ module CCPP_driver
   character(len=256) :: ccpp_suite='undefined'
   character(len=256) :: group_name='undefined'
 
-  public CCPP_step
+  public ccpp_step
 contains
 
-  subroutine CCPP_step(step)
+  subroutine ccpp_step(step)
     character(len=*), intent(in)  :: step
 
-  end subroutine CCPP_step
+    ! CCPP Register phase
+    if (trim(step)=="register") then
+       call ccpp_register(ccpp_suite=trim(ccpp_suite), errmsg=errmsg, errflg=errflg)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_register: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    ! CCPP Init phase
+    else if (trim(step)=="init") then
+       call ccpp_init(ccpp_suite=trim(ccpp_suite), errmsg=errmsg, errflg=errflg)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_init: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    ! CCPP Physics init
+    else if (trim(step)=="physics_init") then
+       call ccpp_physics_init( ccpp_suite=trim(ccpp_suite), group_name='all', &
+            errmsg=errmsg, errflg=errflg, lb=1, ub=1,         &
+            mythread=1, nthreads=1, nphys_threads=1)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_physics_init: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    else if (trim(step)=="timestep_init") then
+       call ccpp_physics_timestep_init(ccpp_suite=trim(ccpp_suite), group_name='all', &
+            errmsg=errmsg, errflg=errflg, lb=1, ub=1,         &
+            mythread=1, nthreads=1, nphys_threads=1)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_physics_timestep_init for group time_vary: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    else if (trim(step)=="radiation" .or. trim(step)=="physics") then
+       call ccpp_physics_run(ccpp_suite=trim(ccpp_suite), group_name="physics", &
+            errmsg=errmsg, errflg=errflg, lb=1, ub=1, &
+            mythread=1, nthreads=1, nphys_threads=1)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_physics_run for group physics: ' // trim(errmsg) // '. Exiting...'
+          return
+       endif
+    else if (trim(step)=="timestep_final") then
+       call ccpp_physics_timestep_final(ccpp_suite=trim(ccpp_suite), group_name="all", &
+            errmsg=errmsg, errflg=errflg, lb=1, ub=1,         &
+            mythread=1, nthreads=1, nphys_threads=1)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_physics_timestep_final group time_vary: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    else if (trim(step)=="physics_final") then
+       call ccpp_physics_final(ccpp_suite=trim(ccpp_suite), group_name='all', &
+            errmsg=errmsg, errflg=errflg, lb=1, ub=1,         &
+            mythread=1, nthreads=1, nphys_threads=1)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_physics_final: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    else if (trim(step)=="final") then
+       call ccpp_final(ccpp_suite=trim(ccpp_suite), errmsg=errmsg, errflg=errflg)
+       if (errflg/=0) then
+          write(error_unit,'(a,i0,a)') 'An error occurred in ccpp_final: ' // trim(errmsg) // '. Exiting...'
+          return
+       end if
+    else
+       write(error_unit,'(2a)') 'Error, undefined CCPP step ', trim(step)
+       return
+    end if
+    
+  end subroutine ccpp_step
 
 end module CCPP_driver
